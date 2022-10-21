ea36edd92189802b3c7530561513d4552a4a4536
Thakur Saad <thakursaad613@gmail.com>
2022-10-21 11:34:50 +0600

    JOB Model & some data changes

--- models/Job.js
+++ models/Job.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
 
 const jobSchema = mongoose.Schema(
   {
@@ -11,25 +12,116 @@ const jobSchema = mongoose.Schema(
       trim: true,
     },
 
-    companyName: {},
+    companyName: {
+      type: String,
+      required: [true, "Please provide a company name"],
+    },
 
-    companyDesc: {},
+    companyDesc: {
+      type: String,
+      required: [true, "Please provide a company description"],
+    },
 
-    jobType: {},
+    jobType: {
+      type: String,
+      required: [true, "Please provide a job type"],
+      enum: {
+        values: ["on-site", "remote", "hybrid"],
+        message: "Job type can not be {VALUE}, must be on-site/remote/hybrid",
+      },
+    },
 
-    jobDesc: {},
+    jobDesc: {
+      type: String,
+      required: [true, "Please provide a job description"],
+    },
 
-    jobField: {},
+    jobField: {
+      type: String,
+      required: [true, "Please provide a job field"],
+      enum: {
+        values: [
+          "engineering",
+          "digital-marketing",
+          "designing",
+          "accounting",
+          "art",
+        ],
+        message:
+          "Job field can not be {VALUE}. Must be art/accounting/designing/digital-marketing/engineering",
+      },
+    },
 
-    jobLocation: {},
+    jobLocation: {
+      type: String,
+      required: [true, "Please provide a job location"],
+    },
 
-    skills: {},
+    skills: {
+      type: String,
+      required: [true, "Please provide skills needed for this job"],
+    },
 
-    startDate: {},
+    startDate: {
+      type: String,
+      required: [true, "Please provide starting date"],
+    },
 
-    salary: {},
+    salary: {
+      type: Number,
+      required: [true, "Please provide salary"],
+    },
 
-    unit: {},
+    unit: {
+      type: String,
+      required: [true, "Please provide a currency for salary"],
+      enum: {
+        values: [
+          "$",
+          "USD",
+          "€",
+          "Euro",
+          "৳",
+          "BDT",
+          "₹",
+          "Rupee",
+          "¥",
+          "Yen",
+          "₩",
+          "Won",
+        ],
+        message:
+          "Unit value can not be '{VALUE}', must be $, USD, €, Euro, ৳, BDT, ₹, Rupee, ¥, Yen, ₩, Won",
+      },
+    },
+
+    deadline: {
+      type: String,
+      required: [true, "Please provide application deadline"],
+    },
+
+    vacancy: {
+      type: Number,
+      required: [true, "Please provide number of vacancies"],
+    },
+
+    candidates: [
+      {
+        type: ObjectId,
+        ref: "User",
+      },
+    ],
+
+    hiringManager: [
+      {
+        name: String,
+        contactNumber: String,
+        id: {
+          type: ObjectId,
+          ref: "User",
+        },
+      },
+    ],
   },
   {
     timestamps: true,
